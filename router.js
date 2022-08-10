0cefe2c7870bbf26d5df46a159cd41d720fd8b56
George Joubran <durd2001@gmail.com>
2022-08-10 18:29:01 +0300

    solved conflict

--- router.js
+++ router.js
@@ -1,14 +1,9 @@
 const httpStatus = require("http-status");
 const checkAuth = require("./middlewares/checkAuth");
 const router = require("express").Router();
-const catchAsync = require("./utils/catchAsync");
 const usersController = require("./controllers/users.controller");
 const matchesController = require("./controllers/matches.controller");
 const predictionsController = require("./controllers/predictions.controller");
-
-const predictionsModel = require("./models/matches.model");
-const teamsModel = require("./models/teams.model");
-const leaguesModel = require("./models/leagues.model");
 
 router.post("/login", usersController.login);
 router.get("/users", usersController.getUsers);
